7e6886f0422871dfda5c3d70f91c770e7c548eb0
Techassi <git@techassi.dev>
2022-06-23 15:49:25 +0200

    Start character selection scene

---
+++ Assets/Scripts/GameLogic/Managers/CharacterManager.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Manager
+{
+  public class CharacterManager : MonoBehaviour
+  {
+    #region declarations
+
+    #endregion
+  }
+}
--- Assets/Scripts/GameLogic/Managers/GameManager.cs
+++ Assets/Scripts/GameLogic/Managers/GameManager.cs
@@ -8,117 +8,129 @@ using UnityEngine.UI;
 
 namespace Manager
 {
-    public class GameManager : MonoBehaviour
+  public class GameManager : MonoBehaviour
+  {
+    /*
+    *
+    *   This is a controller for managing the logic and mechanics of the game.
+    *   Here we should e.g. draw cards, check rules, update scores etc.
+    *   Keeping all gameflow in this singleton class is a Unity Best Practice.
+    *   
+    */
+
+    #region declarations
+    public static GameManager GameManagerInstance; // use this later to access gm from anywhere in our logic
+    private static CardManager CardManagerInstance;
+
+    private static CharacterManager CharacterManagerInstance;
+    private static SceneLoader SceneLoader;
+    private static LevelManager LevelManager;
+    public GameState gameState;
+    public static event Action<GameState> OnGameStateChanged;
+
+    // TODO: implement the individual bits of state for the manager. What states do we need here?
+    public enum GameState
     {
-        /*
-        *
-        *   This is a controller for managing the logic and mechanics of the game.
-        *   Here we should e.g. draw cards, check rules, update scores etc.
-        *   Keeping all gameflow in this singleton class is a Unity Best Practice.
-        *   
-        */
+      StartGame,
+      PlayerTurn,
+      ScoreCalc,
+      TurnEnd,
+      LevelEnd,
+    }
+    #endregion
 
-        #region declarations
-        public static GameManager GameManagerInstance; // use this later to access gm from anywhere in our logic
-        private static CardManager CardManagerInstance;
-        private static SceneLoader SceneLoader;
-        private static LevelManager LevelManager;
-       // private static GameObject UICavas;
-        public GameState gameState;
-        public static event Action<GameState> OnGameStateChanged;
+    #region setup
+    private void Awake()
+    {
 
-        // TODO: implement the individual bits of state for the manager. What states do we need here?
-        public enum GameState
-        {
-            StartGame,
-            PlayerTurn,
-            ScoreCalc,
-            TurnEnd,
-            LevelEnd,
-        }
-        #endregion
+      if (GameManagerInstance != null)
+      {
+        Destroy(this);
+      }
+      else
+      {
+        CharacterManagerInstance = GetComponent<CharacterManager>();
+        CardManagerInstance = GetComponent<CardManager>();
+        LevelManager = GetComponent<LevelManager>();
+        SceneLoader = GetComponent<SceneLoader>();
+        GameManagerInstance = this;
+      }
 
-        #region setup
-        private void Awake()
-        {
+      // Register persistent game objects
+      UnityEngine.Object[] objects = {
+        GameManagerInstance,
+        CardManagerInstance,
+        LevelManager,
+        SceneLoader,
+        GameObject.Find("CardSlots"),
+        GameObject.Find("CardContainer"),
+        GameObject.Find("Canvas"),
+      };
+      RegisterPersistentObjects(objects);
+    }
 
-            if(GameManagerInstance != null)
-            {
-                Destroy(this);
-            }
-            else
-            {
-                GameManagerInstance = this;
-                LevelManager = GetComponent<LevelManager>();
-                SceneLoader = GetComponent<SceneLoader>();
-                LevelManager = GetComponent<LevelManager>();
-                CardManagerInstance = GetComponent<CardManager>();
-            }
+    private void RegisterPersistentObjects(UnityEngine.Object[] objects)
+    {
+      foreach (var obj in objects)
+      {
+        DontDestroyOnLoad(obj);
+      }
+    }
 
-            //TODO: there's a better way for sure
-            DontDestroyOnLoad(GameManagerInstance);
-            DontDestroyOnLoad(CardManagerInstance);
-            DontDestroyOnLoad(SceneLoader);
-            DontDestroyOnLoad(LevelManager);
-            DontDestroyOnLoad(GameObject.Find("CardSlots"));
-            DontDestroyOnLoad(GameObject.Find("CardContainer"));
-            DontDestroyOnLoad(GameObject.Find("Canvas"));
-        }
+    // Start is called before the first frame update
+    void Start()
+    {
+      CardManagerInstance.PopulateDeck();
+      UpdateGameState(GameState.StartGame);
+    }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            CardManagerInstance.PopulateDeck();
-            UpdateGameState(GameState.StartGame);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        void SaveObject()
-        {
-            
-        }
-        #endregion
-
-        #region game state changes
-
-        // TODO: For debugging only, delete!
-        public void ForceScene()
-        {
-            UpdateGameState(GameState.LevelEnd);
-        }
-        public void UpdateGameState(GameState newGameState)
-        {
-            gameState = newGameState;
-            // TODO: Implement handlers for state changes
-            switch (newGameState)
-            {
-                case GameState.StartGame:
-                    LevelManager.UpdateLevelState(LevelManager.LevelState.setUp);
-                    break;
-                case GameState.PlayerTurn:
-                    break;
-                case GameState.ScoreCalc:
-                    break;
-                case GameState.TurnEnd:
-                    break;
-                case GameState.LevelEnd:
-                    SceneLoader.UpdateSceneState(SceneLoader.SceneState.nextScene);
-                    LevelManager.UpdateLevelState(LevelManager.LevelState.setUp);
-                    break;
-                default:
-                    break;
-            }
-
-            OnGameStateChanged?.Invoke(newGameState);
-        }
-        #endregion
-
+    // Update is called once per frame
+    void Update()
+    {
 
     }
+
+    void SaveObject()
+    {
+
+    }
+    #endregion
+
+    #region game state changes
+
+    // TODO: For debugging only, delete!
+    public void ForceScene()
+    {
+      UpdateGameState(GameState.LevelEnd);
+    }
+    public void UpdateGameState(GameState newGameState)
+    {
+      gameState = newGameState;
+      // TODO: Implement handlers for state changes
+      switch (newGameState)
+      {
+        case GameState.StartGame:
+          LevelManager.UpdateLevelState(LevelManager.LevelState.setUp);
+          break;
+        case GameState.PlayerTurn:
+          break;
+        case GameState.ScoreCalc:
+          break;
+        case GameState.TurnEnd:
+          break;
+        case GameState.LevelEnd:
+          SceneLoader.UpdateSceneState(SceneLoader.SceneState.nextScene);
+          LevelManager.UpdateLevelState(LevelManager.LevelState.setUp);
+          break;
+        default:
+          break;
+      }
+
+      OnGameStateChanged?.Invoke(newGameState);
+    }
+    #endregion
+
+
+  }
 }
 
